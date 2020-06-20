0f9bd0b2dfd12ff0e2e715e30ec193c0528f047e
dfuenning <dfuenning@gmail.com>
2020-06-20 12:35:17 -0400

    final working version

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
-
+const db = require("./models");
 const app = express();
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URL || "mongodb://localhost/dbName";
-const options = {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  family: 4 // Use IPv4, skip trying IPv6
-};
-mongoose.connect(MONGODB_URI,options)
+});
 
 // routes
 app.use(require("./routes/api.js"));
